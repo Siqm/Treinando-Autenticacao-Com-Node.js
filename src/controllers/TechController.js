9a6beb1a88baf96844f5df49c8de133bd19662cd
SiqMello <91890617+Siqm@users.noreply.github.com>
2022-09-26 15:17:59 -0300

    Implementando get all techs from user
    Commit 13

--- src/controllers/TechController.js
+++ src/controllers/TechController.js
@@ -2,8 +2,17 @@ const User = require('../models/User')
 const Tech = require('../models/Tech')
 
 module.exports = {
-    async index(req, params) {
+    async index(req, res) {
+        const { user_id } = req.params;
+        const user = await User.findByPk(user_id, {
+            include: { association: 'techs'}
+        });
 
+        if (!user) {
+            return res.status(400).json({ error: 'User not found'})
+        }
+
+        return res.json(user.techs)
     },
 
     async store (req, res) {
